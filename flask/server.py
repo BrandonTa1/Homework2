586bd7fe6e20baad064aa321e98965d8a7328049
Brandon <brandonta21703@gmail.com>
2024-02-22 14:51:46 -0500

    clean up some comments

--- flask/server.py
+++ flask/server.py
@@ -1,12 +1,3 @@
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
-
 from flask import Flask, jsonify
 from flask import render_template
 from flask import request
@@ -19,10 +10,7 @@ app = Flask(__name__)
 def home():
     return "<p>Hello, sqlite3</p>"
 
-# # Route to form used to add a new student to the database
-# @app.route("/enternew")
-# def enternew():
-#     return render_template("student.html")
+
 
 # Route to add a new record (INSERT) student data to the database
 @app.route("/add", methods = ['POST', 'GET'])
@@ -73,27 +61,6 @@ def list():
     # Send the results of the SELECT to the list.html page
     return jsonify(students_list)
 
-# # Route that will SELECT a specific row in the database then load an Edit form 
-# @app.route("/edit", methods=['POST','GET'])
-# def edit():
-#     if request.method == 'POST':
-#         try:
-#             # Use the hidden input value of id from the form to get the rowid
-#             id = request.form['id']
-#             # Connect to the database and SELECT a specific rowid
-#             con = sqlite3.connect("database.db")
-#             con.row_factory = sqlite3.Row
-
-#             cur = con.cursor()
-#             cur.execute("SELECT rowid, * FROM students WHERE rowid = " + id)
-
-#             rows = cur.fetchall()
-#         except:
-#             id=None
-#         finally:
-#             con.close()
-#             # Send the specific record of data to edit.html
-#             return render_template("edit.html",rows=rows)
 
 # Route used to execute the UPDATE statement on a specific record in the database
 @app.route("/edit", methods=['POST','GET'])
@@ -105,12 +72,6 @@ def editrec():
             nm = data['name']
             ID = data['ID']
             points = data['points']
-            # Use the hidden input value of id from the form to get the rowid
-            # rowid = request.form['rowid']
-            # nm = request.form['nm']
-            # addr = request.form['add']
-            # city = request.form['city']
-            # zip = request.form['zip']
 
             # UPDATE a specific record in the database based on the rowid
             with sqlite3.connect('database.db') as con:
@@ -166,11 +127,6 @@ def get_data():
     con.close()
     # Send the results of the SELECT to the list.html page
     return jsonify(students_list)
-    # Your logic to fetch data
-    #data = {"message": "Data fetched successfully", "content": [1, 2, 3, 4, 5]}
-    
-    # Sending the message along with the data in JSON format
-    #return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
